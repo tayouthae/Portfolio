a26cbe24761b7aabcdd50c1a1d39a1b51cc1cff0
tayouthae <tayouthmalla@gmail.com>
2022-07-12 00:17:33 +0545

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-// import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import resume from "../../Assets/../Assets/Tayouth-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-// const resumeLink =
-//   "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+const resumeLink =
+  "https://raw.githubusercontent.com/tayouthae/Portfolio/main/src/Assets/Tayouth-Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -25,8 +25,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            // href={pdf}
-            href
+            href={resume}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -35,23 +34,10 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        {/* <Row className="resume">
+        <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-        </Row> */}
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            // href={pdf}
-            href
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
         </Row>
       </Container>
     </div>
