04cadfed98781a4f643dd46e98428b48a5388b1b
tayouthae <tayouthmalla@gmail.com>
2022-07-12 20:36:45 +0545

    reponsiveness of resume maintained

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,22 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import resume from "../../Assets/../Assets/Tayouth-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-const resumeLink =
-  "https://raw.githubusercontent.com/tayouthae/Portfolio/main/src/Assets/Tayouth-Resume.pdf";
+import resumeImg from '../../Assets/resumeImg.jpg';
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
 
   return (
     <div>
@@ -35,9 +25,9 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <Col md={8}>
+            <img src={resumeImg} alt="about" className="img-fluid" />
+          </Col>
         </Row>
       </Container>
     </div>
